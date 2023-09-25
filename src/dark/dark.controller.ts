07917fde308338528cc54379352a4ad50aa09010
Jourloy <jourloy@yandex.ru>
2023-09-25 17:58:47 +0300

    🔨 Update dark API

--- src/dark/dark.controller.ts
+++ src/dark/dark.controller.ts
@@ -3,10 +3,11 @@ import {DarkService} from "./dark.service";
 import {CreateClassDTO} from "./dto/class.dto";
 import {ERR} from "src/enums/error.enum";
 import {Response} from "express";
-import { JwtGuard } from "src/guards/jwt.guard";
-import { RoleGuard } from "src/guards/role.guard";
-import { Roles } from "src/decorators/roles.decorator";
-import { Role } from "src/enums/role.enum";
+import {JwtGuard} from "src/guards/jwt.guard";
+import {RoleGuard} from "src/guards/role.guard";
+import {Roles} from "src/decorators/roles.decorator";
+import {Role} from "src/enums/role.enum";
+import {CreateAttributeDTO} from "./dto/attribute.dto";
 
 @Controller(`/dark`)
 export class DarkController {
@@ -33,8 +34,34 @@ export class DarkController {
 
 	@Get(`/class/:id`)
 	@UseGuards(JwtGuard)
-	async getClasse(@Param(`id`) id: string, @Res() response: Response) {
+	async getClass(@Param(`id`) id: string, @Res() response: Response) {
 		const state = await this.darkService.getOneClass(+id);
+		response.status(200).json(state);
+	}
+
+	@Post(`/attribute`)
+	@UseGuards(JwtGuard, RoleGuard)
+	@Roles(Role.ADMIN)
+	async createAttribute(@Body() body: CreateAttributeDTO, @Res() response: Response) {
+		const state = await this.darkService.createAttribute(body);
+
+		if (state === ERR.DATABASE) throw new HttpException(ERR.DATABASE, 500);
+		if (state === ERR.DARK_ATTRIBUTE_EXIST) throw new HttpException(ERR.DARK_ATTRIBUTE_EXIST, 400);
+
+		response.status(200).json(state);
+	}
+
+	@Get(`/attribute/all`)
+	@UseGuards(JwtGuard)
+	async getAllAttributes(@Res() response: Response) {
+		const state = await this.darkService.getAllAttributes();
+		response.status(200).json(state);
+	}
+
+	@Get(`/attribute/:id`)
+	@UseGuards(JwtGuard)
+	async getAttribute(@Param(`id`) id: string, @Res() response: Response) {
+		const state = await this.darkService.getOneAttribute(+id);
 		response.status(200).json(state);
 	}
 }
